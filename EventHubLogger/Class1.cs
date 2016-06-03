7e0c8a5eb57a430385c05ae1dc0947afc31f6746
rdziuma <rdziuma@LEBWS049>
2016-06-03 09:44:36 +0300

    Add project files.

---
+++ EventHubLogger/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubLogger
+{
+    public class Class1
+    {
+    }
+}
